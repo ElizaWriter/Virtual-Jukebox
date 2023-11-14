df5625161cebc333b566b939c2eb491c2321e80e
ElizaWriter <144184590+ElizaWriter@users.noreply.github.com>
2023-11-13 16:43:17 -0800

    Update sketch.js

--- Project_3_2023_11_13_04_07_36/sketch.js
+++ Project_3_2023_11_13_04_07_36/sketch.js
@@ -7,26 +7,23 @@ let music5;
 let music6;
 
 function preload() {
-  music1 = loadSound("9 to 5.mp3");
-  music2 = loadSound("Feel Invincible.mp3");
-  music3 = loadSound("Rock This Town.mp3");
-  music4 = loadSound("Happier.mp3");
-  music5 = loadSound("Country Boy (Shake It For Me).mp3");
-  music6 = loadSound("Ballroom Of Romance.mp3");
+  // music1 = loadSound("9 to 5.mp3");
+  // music2 = loadSound("Feel Invincible.mp3");
+  // music3 = loadSound("Rock This Town.mp3");
+  // music4 = loadSound("Happier.mp3");
+  // music5 = loadSound("Country Boy (Shake It For Me).mp3");
+  // music6 = loadSound("Ballroom Of Romance.mp3");
   
 }
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800,600);
   state = 0;
-  music.setVolume(0.5);
   textAlign(CENTER);
   imageMode(CENTER);
-  resetSketch();
 }
 
 function draw() {
-  clear();
   background(255);
   if (state == 0) {
     splashScreen();
@@ -46,10 +43,10 @@ function draw() {
 }
 
 function splashScreen() {
-  music.stop();
   background(255);
   fill(0);
   textSize(60);
+  textFont('Muloka Kerash');
   text("Retro Music Picker", width / 2, height / 4);
   textSize(20);
   text("By Elizabeth Enloe & ", width / 2, height / 3);
@@ -63,51 +60,81 @@ function splashScreen() {
   text("Click to Start", width / 2, height / 2);
 }
 
+function mouseClicked () {
+  state = 1;
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    state = 2;
+  } else if (keyCode === DOWN_ARROW) {
+    state = 3;
+  } else if (keyCode === LEFT_ARROW) {
+    state = 4;
+  } else if (keyCode === RIGHT_ARROW) {
+    state = 5;
+  } else if (keyCode === 32) {
+    state = 6;
+  }
+}
+
 function classicSongScreen() {
-  background(255);
-  music1.play();
+  background(190);
+  //music1.play();
+  //music1.loop = true;
   fill(0);
-  textSize(15);
-  text("Country Boy", width / 2, 20);
+  textSize(60);
+  textFont('DIN');
+  text("9 to 5", width / 2, 60);
 }
 
 function metalSongScreen() {
-  background(255);
-  music2.play();
-  fill(0);
-  textSize(15);
-  text("Feel Invincible", width / 2, 20);
+  background(0);
+  //music2.loop = true;
+  //music2.play();
+  fill(255);
+  textSize(60);
+  textFont('Metalista');
+  text("Feel Invincible", width / 2, 60);
 }
 
 function rockSongScreen() {
-  background(255);
-  music3.play();
+  background(229, 57, 53);
+  //music3.loop = true;
+  //music3.play();
   fill(0);
-  textSize(15);
-  text("Rock This Town", width / 2, 20);
+  textSize(60);
+  textFont('Rockinline');
+  text("Rock This Town", width / 2, 60);
 }
 
 function popSongScreen() {
-  music4.play();
+  background(250, 211, 225);
+  //music4.loop = true;
+  //music4.play();
   fill(0);
-  textSize(15);
-  text("Happier", width / 2, 20);
+  textSize(60);
+  textFont('mvboli');
+  text("Happier", width / 2, 60);
 }
 
 function CountrySongScreen() {
-  music5.play();
+  background(224, 196, 153);
+  //music5.loop = true;
+  //music5.play();
   fill(0);
-  textSize(15);
-  text("Country Boy (Shake It For Me)", width / 2, 20);
+  textSize(60);
+  textFont('barbaro punta');
+  text("Country Boy", width / 2, 60);
+  text("(Shake It For Me)", width / 2, 120)
 }
 
 function CelticSongScreen() {
-  music6.play();
+  background(133, 200, 136);
+  //music6.loop = true;
+  //music6.play();
   fill(0);
-  textSize(15);
-  text("Ballroom of Romance", width / 2, 20);
-}
-
-function resetSketch() {
-  
+  textSize(60);
+  textFont('CELTG');
+  text("Ballroom of Romance", width / 2, 60);
 }
